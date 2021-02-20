a4d847365ef4623d1e1ac1e4bc8c75ceef9fb5c8
nnazo <jacobcurtis2266@gmail.com>
2021-02-20 15:15:45 -0600

    refactor request error handling

--- api/Wrapper.ts
+++ api/Wrapper.ts
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios, { AxiosRequestConfig } from "axios";
 import _ from "lodash";
 
 const apiUrl = process.env.API_URL;
@@ -12,26 +12,46 @@ const api = axios.create({
  * @param city_name The name of the city to create the survey for
  * @param onFail A callback to do any desired error handling
  */
-export const createSurvey = async (
-  city_name?: string,
+export const request = async (
+  config: AxiosRequestConfig,
   onFail: (e: any) => void = console.log
 ) => {
   try {
-    const response = await api.get("/api/create/survey", {
-      params: {
-        city_name: city_name,
-      },
-    });
-
-    if (!_.isUndefined(response.data) && !_.isNull(response.data)) {
-      const surveyId: string = response.data.id;
-      return surveyId;
+    const response = await api.request(config);
+    if (!_.isUndefined(response.data)) {
+      return response.data;
     } else {
-      onFail("");
       return null;
     }
   } catch (e) {
     onFail(e);
     return null;
   }
+};
+
+/**
+ * Create a survey and return the survey ID.
+ *
+ * @param city_name The name of the city to create the survey for
+ * @param onFail A callback to do any desired error handling
+ */
+export const createSurvey = async (
+  city_name?: string,
+  onFail: (e: any) => void = console.log
+) => {
+  const data = await request(
+    {
+      method: "GET",
+      url: "/api/create/survey",
+      params: {
+        city_name: city_name,
+      },
+    },
+    onFail
+  );
+  if (!_.isNull(data)) {
+    const surveyId: string = data.id;
+    return surveyId;
+  }
+  return null;
 };
