254a25ab8b60dfd0660c8938a06ba42ca0c784c9
nnazo <jacobcurtis2266@gmail.com>
2021-02-22 15:37:05 -0600

    add fetchSurveyResults, createSurveyResponse endpoints (todo: annotate types)

--- api/Wrapper.ts
+++ api/Wrapper.ts
@@ -36,7 +36,7 @@ const request = async (
  * @param onFail A callback to do any desired error handling
  */
 export const createSurvey = async (
-  city_name?: string,
+  cityName?: string,
   onFail: (e: any) => void = console.log
 ) => {
   const data = await request(
@@ -44,7 +44,7 @@ export const createSurvey = async (
       method: "GET",
       url: "/api/create/survey",
       params: {
-        city_name: city_name,
+        city_name: cityName,
       },
     },
     onFail
@@ -54,4 +54,60 @@ export const createSurvey = async (
     return surveyId;
   }
   return null;
+};
+
+/**
+ * Fetch the results of a survey.
+ *
+ * @param surveyId The survey's ID we are getting results from
+ * @param onFail A callback to do any desired error handling
+ */
+export const fetchSurveyResults = async (
+  surveyId: string,
+  onFail: (e: any) => void = console.log
+) => {
+  const data = await request(
+    {
+      method: "GET",
+      url: "/api/get/responses",
+      params: {
+        survey_id: surveyId,
+      },
+    },
+    onFail
+  );
+  if (!_.isNull(data)) {
+    // todo: Specify the type of the results array
+    const results: any[] = data.Data;
+    return results;
+  }
+  return null;
+};
+
+/**
+ * Submit all the answers to a survey.
+ *
+ * @param surveyId The survey's ID we are getting results from
+ * @param responses The question responses
+ * @param onFail A callback to do any desired error handling
+ */
+export const createSurveyResponse = async (
+  surveyId: string,
+  responses: any[], // TODO: specify the responses type.  
+  onFail: (e: any) => void = console.log
+) => {
+  const data = await request(
+    {
+      method: "POST",
+      url: `/api/create/response/${surveyId}`,
+      data: responses,
+      headers: {
+        "Content-Type": "application/json"
+      }
+    },
+    onFail
+  );
+  if (!_.isNull(data) && data.status !== "OK") {
+    onFail("Survey response creation failed");
+  }
 };
