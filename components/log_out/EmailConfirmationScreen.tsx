833bc55a9a39323297cc82d7feb730e85e1f211a
nnazo <jacobcurtis2266@gmail.com>
2021-03-13 19:16:20 -0600

    refactor finish button

--- components/log_out/EmailConfirmationScreen.tsx
+++ components/log_out/EmailConfirmationScreen.tsx
@@ -1,32 +1,16 @@
 import React from "react";
-import { Button, Text } from "react-native-paper";
+import { Text } from "react-native-paper";
 import { StyleSheet, View } from "react-native";
+import DoneButton from "./FinishButton";
 
-import { authenticationProps } from "../generate_code/GenerateCodeScreen";
-import { connect } from "react-redux";
-import { mapStateToProps } from "../generate_code/GenerateCodeScreenRedux";
-import mapDispatchToProps from "../generate_code/GenerateCodeScreenD2P";
-
-function EmailConfirmationScreen(props: authenticationProps) {
+export default function EmailConfirmationScreen() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Email has been sent!</Text>
-      <Button
-        mode="contained"
-        onPress={() => {
-          props.updateAuthentication({ isOrganizer: false, id: "" });
-        }}
-      >
-        Done
-      </Button>
+      <DoneButton />
     </View>
   );
 }
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(EmailConfirmationScreen);
 
 const styles = StyleSheet.create({
   container: {
--- components/log_out/SurveyEndScreen.tsx
+++ components/log_out/SurveyEndScreen.tsx
@@ -1,29 +1,16 @@
 import React from "react";
-import { Button, Text } from "react-native-paper";
+import { Text } from "react-native-paper";
 import { StyleSheet, View } from "react-native";
+import FinishButton from "./FinishButton";
 
-import { authenticationProps } from "../generate_code/GenerateCodeScreen";
-import { connect } from "react-redux";
-import { mapStateToProps } from "../generate_code/GenerateCodeScreenRedux";
-import mapDispatchToProps from "../generate_code/GenerateCodeScreenD2P";
-
-function SurveyEndScreen(props: authenticationProps) {
+export default function SurveyEndScreen() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Thank you for asnwering this survey!</Text>
-      <Button
-        mode="contained"
-        onPress={() => {
-          props.updateAuthentication({ isOrganizer: false, id: "" });
-        }}
-      >
-        Exit
-      </Button>
+      <FinishButton />
     </View>
   );
 }
-
-export default connect(mapStateToProps, mapDispatchToProps)(SurveyEndScreen);
 
 const styles = StyleSheet.create({
   container: {
