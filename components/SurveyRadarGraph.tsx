4058289a968188eace32e06983916e14c5fad5dc
Joshua McKone <56418466+ArcturusGrimm@users.noreply.github.com>
2022-01-12 23:53:49 -0600

    Updated Radar Graph weights calculation
    Now has both with only one commented out. Currently has WHO weights selected.

--- components/SurveyRadarGraph.tsx
+++ components/SurveyRadarGraph.tsx
@@ -19,22 +19,40 @@ const computeEssentialAverages = (surveyData: SurveyResponse[][] | null) => {
   // const MAX_VAL = 5;
   const multiplier = 1; //100 / MAX_VAL;
 
+  // WHO weights
   essentialAvgs["Essential 1"] = questionAvgs[0] * multiplier;
+  essentialAvgs["Essential 2"] =
+    ((questionAvgs[1] + questionAvgs[2] + questionAvgs[3]) / 3) * multiplier;
+  essentialAvgs["Essential 3"] = questionAvgs[4] * multiplier;
+  essentialAvgs["Essential 4"] = questionAvgs[5] * multiplier;
+  essentialAvgs["Essential 5"] = questionAvgs[6] * multiplier;
+  essentialAvgs["Essential 6"] =
+    ((questionAvgs[7] + questionAvgs[8] + questionAvgs[9] + questionAvgs[10]) / 4) * multiplier;
+  essentialAvgs["Essential 7"] =
+    ((questionAvgs[11] + questionAvgs[12] + questionAvgs[13]) / 3) * multiplier;
+  essentialAvgs["Essential 8"] =
+    ((questionAvgs[14] + questionAvgs[15] + questionAvgs[16]) / 3) * multiplier;
+  essentialAvgs["Essential 9"] =
+    ((questionAvgs[17] + questionAvgs[18] + questionAvgs[19] + questionAvgs[20]) / 4) * multiplier;
+  essentialAvgs["Essential 10"] = ((questionAvgs[21] + questionAvgs[22]) / 2) * multiplier;
+
+  /*
+  // USDA weights
+  essentialAvgs["Essential 1"] = ((questionAvgs[0] + questionAvgs[1]) / 2) * multiplier;
   essentialAvgs["Essential 2"] =
     ((questionAvgs[2] + questionAvgs[3] + questionAvgs[4]) / 3) * multiplier;
   essentialAvgs["Essential 3"] = questionAvgs[5] * multiplier;
   essentialAvgs["Essential 4"] = questionAvgs[6] * multiplier;
   essentialAvgs["Essential 5"] = questionAvgs[7] * multiplier;
-  essentialAvgs["Essential 6"] =
-    ((questionAvgs[8] + questionAvgs[9]) / 2) * multiplier;
+  essentialAvgs["Essential 6"] = ((questionAvgs[8] + questionAvgs[9]) / 2) * multiplier;
   essentialAvgs["Essential 7"] =
     ((questionAvgs[10] + questionAvgs[11] + questionAvgs[12]) / 3) * multiplier;
   essentialAvgs["Essential 8"] =
     ((questionAvgs[13] + questionAvgs[14] + questionAvgs[15]) / 3) * multiplier;
   essentialAvgs["Essential 9"] =
     ((questionAvgs[16] + questionAvgs[17] + questionAvgs[18]) / 3) * multiplier;
-  essentialAvgs["Essential 10"] =
-    ((questionAvgs[19] + questionAvgs[20]) / 2) * multiplier;
+  essentialAvgs["Essential 10"] = ((questionAvgs[19] + questionAvgs[20]) / 2) * multiplier;
+  */
 
   Object.keys(essentialAvgs).forEach((key) => {
     if (isNaN(essentialAvgs[key])) {
@@ -67,9 +85,7 @@ export default function SurveyRadarGraph(props: Props) {
   useEffect(() => {
     setData(computeEssentialAverages(props.surveyData, props.currentQuestion));
   }, [props.surveyData]);
-  return !_.isNull(props.surveyData) &&
-    data.length > 0 &&
-    Object.keys(data[0]).length > 0 ? (
+  return !_.isNull(props.surveyData) && data.length > 0 && Object.keys(data[0]).length > 0 ? (
     <RadarGraph data={data} />
   ) : (
     <></>
